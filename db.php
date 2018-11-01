0e21c42d47a19718d1c1aa1c11fe62981de2d9ec
Gildeen <gildensofficial@gmail.com>
2018-11-01 13:41:03 +0000

    done

--- db.php
+++ db.php
@@ -5,12 +5,25 @@ if($conn->connect_error){
 }
 
 
-function addUser($fname, $lname, $age, $role){
-    
+function addUser($conn, $fname, $lname, $age, $role){
+    if($stmt = $conn->prepare("INSERT INTO users (fname, lname, age, `role`) VALUES (?,?,?,?)")){
+    $stmt->bind_param("sssi", $fname, $lname, $age, $role);
+    $stmt->execute();
+    $id = $conn->insert_id;
+    echo 'Added ID is: '.$id.'</br>';
+    $stmt->close();
+    } else {
+        die('Error: '.$conn->error);
+    }
 }
 
-function showContent(){
-
+function showContent($conn){
+    $stmt = $conn->prepare("SELECT * FROM users ORDER BY `id` ASC");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while($row = $result->fetch_assoc()){
+        echo $row['id']." | ".$row['fname']." | ".$row['lname']." | ".$row['role'].'</br>';
+    }
 }
 
 ?>
--- index.php
+++ index.php
@@ -4,19 +4,25 @@ include('db.php');
 
 <form method='post' action='#'>
     <label for='fname'>First name: </label>
-    <input type='text' name='fname'/>
+    <input type='text' name='fname'/></br>
     <label for='lname'>Last name: </label>
-    <input type='text' name='lname'/>
+    <input type='text' name='lname'/></br>
     <label for='age'>Age: </label>
-    <input type='text' name='age'/>
+    <input type='text' name='age'/></br>
     <label for='role'>Role: </label>
     <select name='role'>
         <option value='1'>User</option>
         <option value='2'>Admin</option>
         <option value='3'>Super Admin</option>
-    </select>
+    </select></br>
+    <input type='submit' name='submit' value='Submit'>
 </form>
 
 <?php
+    if(isset($_POST['submit'])){
+        
+        addUser($conn, $_POST['fname'], $_POST['lname'], $_POST['age'], $_POST['role']);
+    }
 
+    showContent($conn);
 ?>
